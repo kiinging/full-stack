c6c8d157925905e4874effbb26258471706d871a
wongkiinging <wongkiinging@gmail.com>
2022-03-21 07:44:25 +0000

    Apply CCS styple

--- src/lib/todo-item.svelte
+++ src/lib/todo-item.svelte
@@ -42,22 +42,52 @@
     }
 
     .save {
-        
-    }
+        position: absolute;
+		right: 0;
+		opacity: 0;
+		background-image: url("data:image/svg+xml,%3Csvg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M20.5 2H3.5C2.67158 2 2 2.67157 2 3.5V20.5C2 21.3284 2.67158 22 3.5 22H20.5C21.3284 22 22 21.3284 22 20.5V3.5C22 2.67157 21.3284 2 20.5 2Z' fill='%23676778' stroke='%23676778' stroke-width='1.5' stroke-linejoin='round'/%3E%3Cpath d='M17 2V11H7.5V2H17Z' fill='white' stroke='white' stroke-width='1.5' stroke-linejoin='round'/%3E%3Cpath d='M13.5 5.5V7.5' stroke='%23676778' stroke-width='1.5' stroke-linecap='round'/%3E%3Cpath d='M5.99844 2H18.4992' stroke='%23676778' stroke-width='1.5' stroke-linecap='round'/%3E%3C/svg%3E%0A");
+      }
 
+      .delete {
+    background-image: url("data:image/svg+xml,%3Csvg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M4.5 5V22H19.5V5H4.5Z' fill='%23676778' stroke='%23676778' stroke-width='1.5' stroke-linejoin='round'/%3E%3Cpath d='M10 10V16.5' stroke='white' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/%3E%3Cpath d='M14 10V16.5' stroke='white' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/%3E%3Cpath d='M2 5H22' stroke='%23676778' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/%3E%3Cpath d='M8 5L9.6445 2H14.3885L16 5H8Z' fill='%23676778' stroke='%23676778' stroke-width='1.5' stroke-linejoin='round'/%3E%3C/svg%3E%0A");
+		opacity: 0.2;
+     }
+  
+     .delete:hover,
+  .delete:focus {
+    transition: opacity 0.2s;
+    opacity: 1;
+  }
+  
+
+      .todo input:focus + .save,
+      .save:focus {
+          transition: opacity 0.2s; 
+          opacity: 1;
+      }
+    /* TODO: Uncomment when the API endpoints are available
+    /* .done {
+    transform: none;
+    opacity: 0.4;
+    filter: drop-shadow(0px 0px 1px rgba(0, 0, 0, 0.1));
+  }
+  
+  .done .toggle {
+    background-image: url("data:image/svg+xml,%3Csvg width='22' height='16' viewBox='0 0 22 16' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M20.5 1.5L7.4375 14.5L1.5 8.5909' stroke='%23676778' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/%3E%3C/svg%3E");
+  } */
 </style>
-<div class="todo">
+<div class="todo ">
     <form action="" method="">
         <input type="hidden" name="done" value="" />
-        <button aria-label="Mark done/not done" class="toggle">Done/NotDone</button>
+        <button aria-label="Mark done/not done" class="toggle"></button>
     </form>
 
     <form action="" method="" class="text">
         <input type="text" />
-        <button aria-label="Save todo" class="save">Save</button>
+        <button aria-label="Save todo" class="save"></button>
     </form>
     <form action="" method="">
-        <button aria-label="Delete todo" class="delete">Delete</button>
+        <button aria-label="Delete todo" class="delete"></button>
     </form>
 
 </div>
